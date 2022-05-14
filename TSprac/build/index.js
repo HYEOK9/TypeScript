284b3e37df69eb29386bea474119b5508c2657ce
HYEOK9 <leejaehyuck9@gmail.com>
2022-05-14 23:59:24 +0900

    typeDef

--- TSprac/build/index.js
+++ TSprac/build/index.js
@@ -1,1 +1,10 @@
-const hello = () => 'hello world!';
+let a = 3;
+let b = true;
+let c = [];
+const jaehyeok = {
+    id: 2018102219,
+    name: 'jaehyeok Lee',
+    age: 23,
+    grade: 3,
+};
+console.log(jaehyeok);
--- TSprac/src/index.ts
+++ TSprac/src/index.ts
@@ -1,1 +1,19 @@
-const hello = () => 'hello world!';
+let a = 3;
+let b: boolean = true;
+let c: string[] = [];
+
+type student = {
+    id: number;
+    name: string;
+    age: number;
+    grade: number;
+};
+
+const jaehyeok: student = {
+    id: 2018102219,
+    name: 'jaehyeok Lee',
+    age: 23,
+    grade: 3,
+};
+
+console.log(jaehyeok);
